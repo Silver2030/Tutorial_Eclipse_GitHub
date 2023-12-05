3cf4d4f6d47d7cd51e51e9dc25e84bf18c40ede5
INFORMATICA\ivanrq <ivanrq@educastur.com>
2023-12-05 14:18:31 +0100

    5/12/23

--- clases/Practica 1/hundirLaFlota/Flota.java
+++ clases/Practica 1/hundirLaFlota/Flota.java
@@ -1,65 +1,225 @@
 package hundirLaFlota;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Flota {
-    char caracterSinDestapar;
-    char caracterDestapado;
-	private String[] letras = {"A","B","C","D","E","F","G","H"};
-	private String[][] tablero;
+	public int x;
+    public int y;
+    private String posicion;
+	private String[][] tablero = new String[11][11];
+	private String[] reglas = new String[11];
+	private ArrayList<String> ocupadas = new ArrayList<String>();
+	private ArrayList<String> repetidas = new ArrayList<String>();
+	private ArrayList<String> posfinal = new ArrayList<String>();
 	
 	public Flota() {
 		super();
 	}
-
-	public Flota(int dimension, char caracterSinDestapar, char caracterDestapado, String[] letras, String[][] tablero) {
+	
+	public Flota(int x, int y, String[][] tablero, String[] reglas, ArrayList<String> ocupadas,
+			ArrayList<String> repetidas, ArrayList<String> posfinal) {
 		super();
-		this.caracterSinDestapar = caracterSinDestapar;
-		this.caracterDestapado = caracterDestapado;
-		this.letras = letras;
+		this.x = x;
+		this.y = y;
 		this.tablero = tablero;
+		this.reglas = reglas;
+		this.ocupadas = ocupadas;
+		this.repetidas = repetidas;
+		this.posfinal = posfinal;
 	}
 
-	void imprimeTablero() {
-		tablero = new String[9][9];
-		for(int i = 1; i <= 8; i++) {
-			for(int j = 1; j <= 8; j++) {
-			tablero[i][j] = letras[i-1] + String.valueOf(j);
+	public void creartablero() {
+			int letras=65;
+	        int num=49;
+	        char numeros=(char)49;
+	        char letra=(char)65;
+	        for(int i=1;i<11;i++){
+	            tablero[0][i]=Character.toString(numeros);
+	            num=num+1;
+	            numeros=(char)num;
+	        }
+	        for(int i=1;i<11;i++){
+	            tablero[i][0]=Character.toString(letra);
+	            letras=letras+1;
+	            letra=(char)letras;
+	        }
+	  
+	        for(int i = 1; i < 11; i++) {
+				for(int j = 1; j <= 10; j++) {
+				tablero[i][j] = "*";
+				}
 			}
-		}
-		
-		for(int i=1; i <= 8; i++)
-        {
-            for(int j=1; j <= 8; j++)
-            {
-                System.out.print(tablero[j][i] + "\t");
+	        
+	        tablero[0][10]="10";
+	        tablero[0][0]="";
+	}
+	
+	public  void mostartablero(){
+        for(int i=0;i<11;i++){
+            for(int j=0;j<11;j++){
+                System.out.print(tablero[i][j]+"\t");
             }
-            System.out.println("\n");
+            System.out.println("\t"+"\n");
         }
     }
 	
-	public char getCaracterSinDestapar() {
-		return caracterSinDestapar;
+	public void crearreglas(){
+        reglas[0]="			Reglas del juego Hundir la Flota";
+        reglas[1]="El juego se terminara una vez descubras la posicion de todos los barcos enemigos o se acaben los 25 intentos";
+        reglas[2]="Cuando se te pida que digas una posición, tienes que decir una letra y un numero";
+        reglas[3]="Simbología: ";
+        reglas[4]="	* - Cuadro oculto: Se desconoce que se encuentra debajo de él";
+        reglas[5]="	- - Agua: El cuadro ha sido atacado pero no se ha logrado tocar nada";
+        reglas[6]="	+ - Tocado: El cuadro ha sido atacado y se ha logrado dar con una parte de barco";
+        reglas[7]="	X - Hundido: El cuadro ha sido atacado y se ha logrado acabar con todo el barco";
+        reglas[8]="Composición de la Flota:";
+        reglas[9]="	1 Portaaviones(Ocupa 4 espacios), 2 Acorazados(3 posiciones cada uno)";
+        reglas[10]="	3 Buques(2 posiciones cada uno) y 4 Submarinos(1 posicion cada uno)";
+    }
+	
+	public void mostrarreglas() {
+		for(int i=0; i<11;i++) {
+			System.out.println("\t"+reglas[i]+"\n");
+		}
+		System.out.println();
+	}
+	
+	public int letranum(String user) {
+		if("A".equals(user.substring(0,1))){
+            return 1;
+        }else if("B".equals(user.substring(0,1))){
+            return 2;
+        }else if("C".equals(user.substring(0,1))){
+            return 3;
+        }else if("D".equals(user.substring(0,1))){
+            return 4;
+        }else if("E".equals(user.substring(0,1))){
+            return 5;
+        }else if("F".equals(user.substring(0,1))){
+            return 6;
+        }else if("G".equals(user.substring(0,1))){
+            return 7;
+        }else if("H".equals(user.substring(0,1))){
+            return 8;
+        }else if("I".equals(user.substring(0,1))){
+            return 9;
+        }else if("J".equals(user.substring(0,1))){
+            return 10;
+        }else return 0;	
+	}
+	
+    public void genenarbarcos(int logitud){
+        int x=(int) (Math.random()*10+1);
+        int y=(int) (Math.random()*10+1);
+        int orientacion=(int) (Math.random()*2+1);
+        String orientado="";
+        if(x<5 && y<5){
+            if(orientacion==1){
+                orientado="derecha";
+            }else{
+                orientado="abajo";
+            }
+        }else if(x>5 && y>5){
+            if(orientacion==1){
+                orientado="izquierda";
+            }else{
+                orientado="arriba";
+            }
+        }else if(x>5 && y<5){
+            orientado="arriba";
+        }else if(x<5 && y>5){
+            orientado="abajo";
+        }else{
+            if(orientacion==1){
+                orientado="izquierda";
+            }else{
+                orientado="arriba";
+            }
+        }
+        generarposiciones(x,y,orientado,logitud);
+    }
+    
+    public void generarposiciones(int x, int y, String orientado, int longitud){
+        String posicionesfinal="";
+        int conterr=0;
+        if(orientado=="derecha"){
+            for(int o=y;o<y+longitud;o++){
+                if(comprobar(x,o)==true){
+                    conterr=1;
+                    repetidas.add(Integer.toString(x)+","+Integer.toString(o));
+                }else{
+                    ocupadas.add(Integer.toString(x)+","+Integer.toString(o));
+                    posicionesfinal=posicionesfinal+Integer.toString(x)+","+Integer.toString(o)+" ";
+                }
+            }
+        }
+        if(orientado=="izquierda"){
+            for(int o=y;o>y-longitud;o--){
+                if(comprobar(x,o)==true){
+                    conterr=1;
+                    repetidas.add(Integer.toString(x)+","+Integer.toString(o));
+                }else{
+                    ocupadas.add(Integer.toString(x)+","+Integer.toString(o));
+                    posicionesfinal=posicionesfinal+Integer.toString(x)+","+Integer.toString(o)+" ";  
+                }
+            }
+        }
+        if(orientado=="abajo"){
+            for(int o=x;o<x+longitud;o++){
+                if(comprobar(o,x)==true){
+                    conterr=1;
+                    repetidas.add(Integer.toString(x)+","+Integer.toString(o));
+                }else{
+                    ocupadas.add(" "+Integer.toString(o)+","+Integer.toString(x));
+                    posicionesfinal=posicionesfinal+Integer.toString(o)+","+Integer.toString(x)+" ";
+                }
+            }
+        }
+        if(orientado=="arriba"){
+            for(int o=x;o>x-longitud;o--){
+                if(comprobar(o,x)==true){
+                    conterr=1;
+                    repetidas.add(Integer.toString(x)+","+Integer.toString(o));
+                }else{
+                    ocupadas.add(Integer.toString(o)+","+Integer.toString(x));
+                    posicionesfinal=posicionesfinal+Integer.toString(o)+","+Integer.toString(x)+" "; 
+                }
+            }
+        }
+        if (conterr==1){
+            genenarbarcos(longitud);
+        }else{
+            posfinal.add(posicionesfinal);
+        }
+    }
+    
+    public boolean comprobar(int x,int o){
+        String pos = Integer.toString(x)+","+Integer.toString(o);
+        String pos2 = Integer.toString(x)+","+Integer.toString(o)+",";
+        String pos3 = " "+Integer.toString(x)+","+Integer.toString(o)+",";
+        String pos4 = " "+Integer.toString(x)+","+Integer.toString(o);
+        if(ocupadas.contains(pos)==true || ocupadas.contains(pos2)==true || ocupadas.contains(pos3)==true ||ocupadas.contains(pos4)==true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+	public int getX() {
+		return x;
 	}
 
-	public void setCaracterSinDestapar(char caracterSinDestapar) {
-		this.caracterSinDestapar = caracterSinDestapar;
+	public void setX(int x) {
+		this.x = x;
 	}
 
-	public char getCaracterDestapado() {
-		return caracterDestapado;
+	public int getY() {
+		return y;
 	}
 
-	public void setCaracterDestapado(char caracterDestapado) {
-		this.caracterDestapado = caracterDestapado;
-	}
-
-	public String[] getLetras() {
-		return letras;
-	}
-
-	public void setLetras(String[] letras) {
-		this.letras = letras;
+	public void setY(int y) {
+		this.y = y;
 	}
 
 	public String[][] getTablero() {
@@ -69,6 +229,26 @@ public class Flota {
 	public void setTablero(String[][] tablero) {
 		this.tablero = tablero;
 	}
+
+	public String[] getReglas() {
+		return reglas;
+	}
+
+	public void setReglas(String[] reglas) {
+		this.reglas = reglas;
+	}
+
+	public String getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(String posicion) {
+		this.posicion = posicion;
+	}
+	
+	
+	
+
 
 
 }
--- clases/Practica 1/hundirLaFlota/FlotaApp.java
+++ clases/Practica 1/hundirLaFlota/FlotaApp.java
@@ -1,21 +1,65 @@
 package hundirLaFlota;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class FlotaApp {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		Flota obj = new Flota();
-		String c;
-		String[][] prueba;
+		char res;
+		int num, intentos = 0;
+		String[] letras = {"A","B","C","D","E","F","G","H"};
+		String[][] valor = new String[9][9];
+		ArrayList<String> barcos = new ArrayList<String>();
 		
-		obj.imprimeTablero();
-		
-		System.out.println("Probando");
-		c = in.nextLine();
-		
-		if()
+		obj.crearreglas();
+		obj.mostrarreglas();
+		obj.creartablero();
+		obj.mostartablero();
+		do {
+		System.out.println("Introduce una casilla, primero letra despues numero");
+		res = in.next().charAt(0);
+		obj.setX(obj.letranum(Character.toString(res)));
+		num = in.nextInt();
+		obj.setY(num);
+		String posxy= obj.getX()+","+ obj.getY();
+        System.out.println(posxy);
+		if((obj.getX() == 0)||((obj.getY()>11)&&(obj.getY()<1))) {
+			System.out.print("Respuesta invalida, por favor vuelta a intentarlo: ");
+		}else{
+            boolean comprobacion = false;
+            for(int i=0;i<barcos.size();i++){
+                String barcopos = barcos.get(i);
+                if(barcopos.contains(posxy)==true){
+                	tablero[posx][posy]="+";
+                    System.out.println("Bien has dado a un barco");
+                    comprobacion=true;
+                    barcos.get(i).tocado(x, y);
+                    if(barcos.get(i).posiciones()==barcos.get(i).getTamano()){
+                        System.out.println("Hundido");
+                        System.out.println("Has hundido un "+barcos.get(i).getNombre());
+                        for(int o=0;o<barcos.get(i).posicionesx.size();o++){
+                            tablero[barcos.get(i).posicionesx.get(o)][barcos.get(i).posicionesy.get(o)]="X";
+                        }
+                        barcos.remove(i);
+                    }
+                }
+            }
+            if(comprobacion==false){
+                System.out.println("Agua");
+                obj.setTablero();
+            }
+        }
+        obj.mostartablero();
+        intentos ++;
+        if(barcos.size()==0){
+            break;
+        }
+       
+		}while(intentos!=25);
 		
 	}
+	
 }
